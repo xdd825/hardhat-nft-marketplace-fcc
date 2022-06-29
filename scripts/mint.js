3386b84d359621a4084d41f70ff0e7ff05e7fd38
Ben Force <xdd741852963@gmail.com>
2022-06-29 13:46:39 +0800

    add mint script

---
+++ scripts/mint.js
@@ -0,0 +1,25 @@
+const { ethers, network } = require("hardhat")
+const { moveBlocks } = require("../utils/move-blocks")
+
+async function mint() {
+    const basicNft = await ethers.getContract("BasicNft")
+    console.log("Minting NFT...")
+    const mintTx = await basicNft.mintNft()
+    const mintTxReceipt = await mintTx.wait(1)
+    const tokenId = mintTxReceipt.events[0].args.tokenId
+    console.log(`Got tokenId ${tokenId}`)
+    console.log(`NFT Address: ${basicNft.address}`)
+
+    if (network.config.chainId == "31337") {
+        await moveBlocks(1, (sleepAmount = 1000))
+    }
+}
+
+mint()
+    .then(() => {
+        process.exit(0)
+    })
+    .catch((error) => {
+        console.log(error)
+        process.exit(1)
+    })
